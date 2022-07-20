f9efbc0c41d939af4b532c07ae57ec17e6270add
Janko Jelic <janko.jelic99@gmail.com>
2022-07-20 22:04:08 +0200

    Update Keyboard.tsx

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -4,12 +4,19 @@ import React from "react";
 const Keyboard = () => {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+  const Letter = ({ letter }: { letter: string }) => (
+    <div
+      className="flex w-8 aspect-square rounded-lg m-4 
+      cursor-pointer bg-mainExtraLight items-center justify-center"
+    >
+      {letter}
+    </div>
+  );
+
   return (
-    <div className="flex flex-wrap p-4 mx-4">
+    <div className="flex flex-wrap p-4 mx-4 mt-8">
       {alphabet.split("").map((letter) => (
-        <div className="flex w-8 aspect-square rounded-lg m-4 cursor-pointer bg-mainExtraLight items-center justify-center">
-          {letter}
-        </div>
+        <Letter letter={letter} />
       ))}
     </div>
   );
