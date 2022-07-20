c76b72cc035c8f5d6e2ad77cce971c1606071d73
Janko Jelic <janko.jelic99@gmail.com>
2022-07-20 21:07:38 +0200

    type Puzzle, Hangnman started

---
+++ src/containers/Hangman/Hangman.tsx
@@ -0,0 +1,10 @@
+const Hangman = () => {
+  return (
+    <div className="border-b-4 w-72 sm:w-96 border-mainText h-52 relative pl-6">
+      <div className="w-1 bg-mainText h-52" />
+      <div className="h-1 bg-mainText absolute top-0 w-32" />
+    </div>
+  );
+};
+
+export default Hangman;
--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -1,19 +1,31 @@
+import Hangman from "containers/Hangman";
 import { appContext } from "context";
 import { useContext, useState } from "react";
 import { useLocation } from "react-router-dom";
 
+type Puzzle = {
+  author: string;
+  content: string;
+  length: number;
+};
+
 const Game = () => {
-  const { state } = useLocation();
+  const location = useLocation();
   const appState = useContext(appContext);
+
   const [startTime, setStartTime] = useState(Date.now());
   const [endTime, setEndTime] = useState(Date.now());
   const [numberOfMistakes, setNumberOfMistakes] = useState(0);
 
+  const { data } = location.state as { data: Puzzle };
+
   return (
-    <main style={{ padding: "1rem 0" }}>
+    <div className="flex flex-col items-center pt-10">
+      <Hangman />
+      <h2 className="font-semibold mt-4">A wise man once said:</h2>
       <h1>{appState.playerName}</h1>
       <h2>Game</h2>
-    </main>
+    </div>
   );
 };
 
