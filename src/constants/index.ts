a0dd9a76de126e040afc3b1737b0224a8d66e2a2
Janko Jelic <janko.jelic99@gmail.com>
2022-07-22 00:57:53 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const PUZZLE_URL = "https://api.quotable.io/random";
+export const PUZZLE_URL = "http://api.quotable.io/random";
 export const HIGHSCORE_URL =
   "https://my-json-server.typicode.com/stanko-ingemark/hang_the_wise_man_frontend_task/highscores";
 
