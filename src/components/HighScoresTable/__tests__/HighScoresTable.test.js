3c545c419a539d1506c3a5fe846f81016932ff52
Janko Jelic <janko.jelic99@gmail.com>
2022-07-21 19:57:48 +0200

    Highscore data table test

--- src/components/HighScoresTable/__tests__/HighScoresTable.test.js
+++ src/components/HighScoresTable/__tests__/HighScoresTable.test.js
@@ -1,2 +1,50 @@
 import HighScoresTable from "../HighScoresTable";
+import renderer from "react-test-renderer";
+import { render, screen } from "@testing-library/react";
 
+const mockData = [
+  {
+    id: 1,
+    length: 20,
+    userName: "janko",
+    duration: 100000,
+    quoteId: "e2ni",
+    errors: 10,
+    uniqueCharacters: 17,
+  },
+  {
+    id: 2,
+    length: 22,
+    userName: "jakov",
+    duration: 222222,
+    quoteId: "e22ni",
+    errors: 9,
+    uniqueCharacters: 14,
+  },
+  {
+    id: 3,
+    length: 99,
+    userName: "user3",
+    duration: 88888,
+    quoteId: "e12ni",
+    errors: 3,
+    uniqueCharacters: 20,
+  },
+];
+
+it("renders correctly", () => {
+  const tree = renderer.create(<HighScoresTable data={mockData} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("has 6 columns", () => {
+  render(<HighScoresTable data={mockData} />);
+
+  expect(screen.getAllByRole("columnheader")).toHaveLength(6);
+});
+
+it("its body contains one row for each item in data", () => {
+  render(<HighScoresTable data={mockData} />);
+
+  expect(screen.getAllByRole("cell")).toHaveLength(6 * mockData.length);
+});
