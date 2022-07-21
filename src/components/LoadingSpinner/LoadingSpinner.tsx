82f203759eae95245bd280194d89c59c92864eae
Janko Jelic <janko.jelic99@gmail.com>
2022-07-21 13:08:48 +0200

    Update LoadingSpinner.tsx

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,11 +1,9 @@
 import BackdropOverlay from "components/BackdropOverlay";
-import useDimensions from "hooks/useDimensions";
 import { useSelector } from "react-redux";
 import { RootState } from "store/store";
 import { ReactComponent as Spinner } from "../../assets/spinner.svg";
 
 const LoadingSpinner = () => {
-  const { width, height } = useDimensions();
   const visible = useSelector((state: RootState) => state.appState.isLoading);
   console.log(visible);
   if (!visible) return <></>;
