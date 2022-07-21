a1d2244e4b9df356e67c4d68947813778ee53479
Janko Jelic <janko.jelic99@gmail.com>
2022-07-21 22:05:27 +0200

    NameForm test

---
+++ src/containers/NameForm/__tests__/NameForm.test.js
@@ -0,0 +1,45 @@
+import NameForm from "../NameForm";
+import { render, screen } from "test-utils";
+import { fireEvent } from "test-utils";
+
+const renderNameForm = () => render(<NameForm />);
+
+it("contains correct elements", () => {
+  renderNameForm();
+
+  expect(screen.getByText("Tell us your name!")).toBeInTheDocument();
+  expect(screen.getByRole("textbox")).toBeInTheDocument();
+  expect(screen.getByRole("button")).toBeInTheDocument();
+  expect(screen.getByText("Enter")).toBeInTheDocument();
+});
+
+describe("button", () => {
+  it("is disabled when input field is empty", () => {
+    renderNameForm();
+
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
+});
+
+describe("input field", () => {
+  it("displays correct text entry and enables the button when has entry", () => {
+    renderNameForm();
+
+    const inputField = screen.getByRole("textbox");
+
+    fireEvent.change(inputField, { target: { value: "asdf" } });
+
+    expect(inputField.value).toBe("asdf");
+    expect(screen.getByRole("button")).not.toBeDisabled();
+  });
+
+  it("parses the input so only letters are accepted", () => {
+    renderNameForm();
+
+    const inputField = screen.getByRole("textbox");
+
+    fireEvent.change(inputField, { target: { value: "asdf23" } });
+
+    expect(inputField.value).toBe("asdf");
+  });
+});
--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -2,9 +2,24 @@ import React, { FC, ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import store from "store/store";
 import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
 
 const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <Router
+        location="/"
+        navigator={{
+          push: () => {},
+          createHref: () => "/",
+          replace: () => {},
+          go: () => {},
+        }}
+      >
+        {children}
+      </Router>
+    </Provider>
+  );
 };
 
 const customRender = (
